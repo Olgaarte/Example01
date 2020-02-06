dc3dda6543d73fba69ddb917447aa60d0c3c4509
Olgaarte <olga.hapon@gmail.com>
2020-02-06 14:10:48 +0200

    tree test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,23 +2,29 @@
 
 namespace ConsoleApp1
 {
-    class Program
+    public class  Program
     {
+		static public int Example(int a, int b)
+		{
+			int c = a + b;
+
+			Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + c.ToString());
+
+			return c;
+		}
+		
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите первое число");
-            
+			Console.WriteLine("Введите первое число");
+
 			string S1 = Console.ReadLine();
 			int a = int.Parse(S1);
-			 
+
 			Console.WriteLine("Введите второе число");
 			string S2 = Console.ReadLine();
 			int b = int.Parse(S2);
-			 
-			int c = a + b;
-			 
-			Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + c.ToString());
-			 
+
+			Example(a, b);
 			Console.ReadLine();
 
         }
